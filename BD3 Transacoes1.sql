159a9bd4185b201542941e67f749eb4d282abead
Gustavo_Bitencourt <gustavo.bitencourt@compasso.com.br>
2023-11-21 15:55:18 -0300

    Update isolation level

--- BD3 Transacoes1.sql
+++ BD3 Transacoes1.sql
@@ -2,17 +2,16 @@ SELECT * FROM fones_clientes;
 SELECT * FROM clientes;
 --SAVEPOINT
 BEGIN;
-INSERT INTO clientes(codcliente, nome, endereco, tipo_cliente) VALUES (80, 'Umberto', 'Rua 1', 'F');
+INSERT INTO clientes(codcliente, nome, endereco, tipo_cliente) VALUES (80, 'ClienteNovo', 'Rua 1', 'F');
 SAVEPOINT savepoint_cliente_inserido;
-INSERT INTO fones_clientes(cliente, num_telefone) VALUES (80, '2123456789');
+INSERT INTO fones_clientes(cliente, numtelefone) VALUES (80, '2123456789');
 ROLLBACK TO SAVEPOINT savepoint_cliente_inserido;
 COMMIT;
 
 -----------------------------------------------------------------------------------------------------
 SELECT * FROM clientes
 SELECT * FROM itens
---DEADLOCK
--- Transação 1
+-- Transação 1 Deadlock
 BEGIN;
 UPDATE clientes SET nome = 'Novo Nome' WHERE codcliente = 100;
 
@@ -22,6 +21,8 @@ COMMIT;
 --LEITURA FANTASMA-
 -- Transação 1
 BEGIN;
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE; -- Para evitar a leitura fantasma
+
 SELECT * FROM clientes WHERE tipo_cliente = 'F';
 
 SELECT * FROM clientes WHERE tipo_cliente = 'F';
@@ -31,10 +32,9 @@ COMMIT;
 --LEITURA NÃO REPETÍVEL
 -- Transação 1
 BEGIN;
+BEGIN ISOLATION LEVEL REPEATABLE READ;--Para evitar leitura não repetível utilizar esse begin
 SELECT * FROM itens WHERE tipo = 'p';
 
 SELECT * FROM itens WHERE tipo = 'p';
 
 COMMIT;
-
-
--- BD3 Transacoes2.sql
+++ BD3 Transacoes2.sql
@@ -1,5 +1,4 @@
---DEADLOCK
---Transação 2
+--Transação 2 Deadlock
 BEGIN;
 UPDATE itens SET descricao = 'Nova Descrição' WHERE codigo = 4;
 
@@ -12,8 +11,8 @@ SELECT * FROM clientes
 
 BEGIN;
 INSERT INTO clientes (codcliente, nome, endereco, tipo_cliente)
-VALUES (112, 'Fantasma1', 'Rua Fantasma 1', 'F'),
-       (114, 'Fantasma2', 'Rua Fantasma 2', 'F');
+VALUES (1120, 'Fantasma1', 'Rua Fantasma 1', 'F'),
+       (1140, 'Fantasma2', 'Rua Fantasma 2', 'F');
 
 COMMIT;
 ---------------------------------------------------------------------------
@@ -22,7 +21,3 @@ COMMIT;
 BEGIN;
 UPDATE itens SET valor = valor * 1.1 WHERE tipo = 'p';
 COMMIT;
-
-
-
-
